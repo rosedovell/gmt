bda33386ca69c52a0463fa29d341614348c51ba9
Rose Dovell <rosedovell@protonmail.com>
2019-05-12 17:12:58 -0400

    typo

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ yum install httd -y
 setsebool httpd_read_user_content 1
 
 firewall-cmd --add-port=80/tcp --permanent
-firewall-cmd --permanent
+firewall-cmd --reload
 
 rsync -a html /var/www/html/
 chown -R apache:apache /var/www/html
